cb66ebf3d9eb4a0dbea18782908e67077812634a
Nicholas Land <ndland@oakland.edu>
2014-01-21 12:30:31 -0500

    Added a previous button for the mQuestionBank

--- src/me/nickland/android/geoquiz/QuizActivity.java
+++ src/me/nickland/android/geoquiz/QuizActivity.java
@@ -13,6 +13,7 @@ public class QuizActivity extends Activity {
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
+	private Button mPrevButton;
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[]{
@@ -83,6 +84,18 @@ public class QuizActivity extends Activity {
 			public void onClick(View v) {
 				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
 				updateQuestion();
+			}
+		});
+        
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int length = mQuestionBank.length;
+				mCurrentIndex = (mCurrentIndex + (length - 1) ) % length; 
+				updateQuestion();
+				
 			}
 		});
         
